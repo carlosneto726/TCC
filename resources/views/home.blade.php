d0122cf9438d3063b08af762afc67f5deb3a01ad
Caique <carlosneto726@gmail.com>
2023-06-30 20:01:47 -0300

    definindo a base do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('templates.template')
+@section('content')
+
+<center style="margin-top: 400px">
+
+    <h1 id="test">Site em construção</h1>
+
+</center>
+
+@endsection
---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css\app.css")}}">
+    <title>Cooperativas Unidas</title>
+</head>
+<body>
+
+    <header>
+        <!-- @//include('layout.navbar') -->
+    </header>
+
+    <section>
+        @yield('content')
+    </section>
+
+    <footer>
+        <!-- @//include('layout.footer') -->
+    </footer>
+    
+    
+</body>
+</html>
