320afcd85257011fd762e483a0863a832bf9e4ed
Caique <carlosneto726@gmail.com>
2023-07-02 21:37:14 -0300

    adicionando uma nova função de rota

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        return view('home');
+    }
+
+    public function carrinho(){
+        return view('carrinho');
+    }
 }
