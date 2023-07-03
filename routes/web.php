4d93f272d5e30d50f7a2e3013d4a34a8605cbfdc
Caique <carlosneto726@gmail.com>
2023-07-02 21:37:43 -0300

    adicionando a rota para o carrinho

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'App\Http\Controllers\UserController@index');
+Route::get('/carrinho', 'App\Http\Controllers\UserController@carrinho');
