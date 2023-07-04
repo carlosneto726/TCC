8d70e59404d327b9919697515db31779097d96b0
Caique <carlosneto726@gmail.com>
2023-07-03 23:34:05 -0300

    adicionando novas funções de rotas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,25 @@ class UserController extends Controller
     public function carrinho(){
         return view('carrinho');
     }
+
+    public function pesquisa(){
+        $pesquisa = request("pesquisa");
+        return view('pesquisa', compact('pesquisa'));
+    }
+
+    public function login(){
+        return view('login_cadastro.login');
+    }
+
+    public function cadastro(){
+        return view('login_cadastro.cadastro');
+    }
+
+    public function cadastro_usuario(){
+        return view('login_cadastro.cadastro_usuario');
+    }
+    
+    public function cadastro_cooperativa(){
+        return view('login_cadastro.cadastro_cooperativa');
+    }
 }
