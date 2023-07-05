7c812fab08d9ec918a0109f3f61f005af3e4ee00
Caique <carlosneto726@gmail.com>
2023-07-04 23:20:49 -0300

    adicionando rotas para criar usuarios e cooperandos

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,11 @@ Route::get('/pesquisa', 'App\Http\Controllers\UserController@pesquisa');
 Route::get('/login', 'App\Http\Controllers\UserController@login');
 Route::get('/cadastro', 'App\Http\Controllers\UserController@cadastro');
 Route::get('/cadastro/usuario', 'App\Http\Controllers\UserController@cadastro_usuario');
+Route::post('/cadastro/usuario', 'App\Http\Controllers\UserController@cadastrar_usuario');
+
 Route::get('/cadastro/cooperativa', 'App\Http\Controllers\UserController@cadastro_cooperativa');
+Route::post('/cadastro/cooperativa', 'App\Http\Controllers\UserController@cadastrar_cooperativa');
+
+Route::get('/produto', 'App\Http\Controllers\UserController@pagina_produto');
+Route::get('/cooperativa', 'App\Http\Controllers\UserController@cooperativa');
 
