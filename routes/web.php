4cad08e4f8c4558443874a44f6a706d7fca8a4d2
Caique <carlosneto726@gmail.com>
2023-07-05 10:50:31 -0300

    organizando as rotas

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rotas do vistitante
 Route::get('/', 'App\Http\Controllers\UserController@index');
-Route::get('/carrinho', 'App\Http\Controllers\UserController@carrinho');
 Route::get('/pesquisa', 'App\Http\Controllers\UserController@pesquisa');
+Route::get('/produto', 'App\Http\Controllers\UserController@pagina_produto');
+Route::get('/cooperativa', 'App\Http\Controllers\UserController@cooperativa');
+
 Route::get('/login', 'App\Http\Controllers\UserController@login');
 Route::get('/cadastro', 'App\Http\Controllers\UserController@cadastro');
 Route::get('/cadastro/usuario', 'App\Http\Controllers\UserController@cadastro_usuario');
-Route::post('/cadastro/usuario', 'App\Http\Controllers\UserController@cadastrar_usuario');
-
 Route::get('/cadastro/cooperativa', 'App\Http\Controllers\UserController@cadastro_cooperativa');
+
+Route::post('/cadastro/usuario', 'App\Http\Controllers\UserController@cadastrar_usuario');
 Route::post('/cadastro/cooperativa', 'App\Http\Controllers\UserController@cadastrar_cooperativa');
 
-Route::get('/produto', 'App\Http\Controllers\UserController@pagina_produto');
-Route::get('/cooperativa', 'App\Http\Controllers\UserController@cooperativa');
+
 
