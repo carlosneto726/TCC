3c26e30a92dc5189af55caac52240155e0f3934d
carlosneto726 <carlosneto726@gmail.com>
2023-09-09 17:04:31 -0300

    embelezando a aba de carrinhos

--- resources/views/carrinho/carrinho.blade.php
+++ resources/views/carrinho/carrinho.blade.php
@@ -1,103 +1,105 @@
 @extends('templates.template')
 @section('content')
 
-<h2 class="container">Meu carrinho</h2>
+<div class="container">
+    <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="{{asset("icons/cart.svg")}}" width="72" height="57">
+        <h2>Seu Carrinho</h2>
+    </div>
 
-<div class="container mt-5">
-    <div class="hstack">
-        <div class="container me-3 p-3 mb-auto rounded" style="background-color: var(--light-gray);">
-            @if (count($produtos) == 0)
-                <h5>Parece que você ainda não tem nenhum produto no carrinho. Veja o nosso <a href="{{url("/")}}">catálogo</a></h5>                
-            @endif
-
-            @foreach ($produtos as $produto)
-                <div class="card mb-3" style="max-width: 1200px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="{{asset("storage/".$produto->imagem)}}" class="img-fluid rounded" style="height: 150px; object-fit: contain;">
+    <div class="row g-5">
+        <div class="col-md-5 col-lg-4 order-md-last">
+            <h4 class="d-flex justify-content-between align-items-center mb-3">
+                <span class="text-success">Resumo do pedido</span>
+                <span class="badge bg-success rounded-pill">{{$quantidade_produtos}}</span>
+            </h4>
+            <ul class="list-group mb-3">
+                @foreach ($produtos as $produto)
+                    <li class="list-group-item d-flex justify-content-between lh-sm">
+                        <div>
+                            <h6 class="my-0">{{$produto->nome}}</h6>
+                            <small class="text-body-secondary">{{$produto->descricao}}</small>
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{$produto->nome}}</h5>
-                                <p class="card-text text-truncate">{{$produto->descricao}}</p>
-                                <div class="d-flex">
-                                    <form action="{{url("/carrinho/update")}}" method="POST">
-                                        @csrf
-                                        @method("POST")
-                                        <input type="text" name="id_produto" value="{{$produto->pid}}" hidden>
-                                        <div class="dropdown">
-                                            <button class="btn border border-1 border-dark-subtle dropdown-toggle ms-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                {{$produto->qtd}}
-                                            </button>
-                                            <ul class="dropdown-menu">
-                                                @for ($i = 1; $i < $produto->pqtd; $i++)
-                                                    <li>
-                                                        <button class="dropdown-item" type="submit" name="quantidade" value="{{$i}}">
-                                                            {{$i}}
-                                                        </button>
-                                                    </li>
-                                                    @if($i == 9)
-                                                        @break
-                                                    @endif
-                                                @endfor
-                                            </ul>
-                                        </div>
-                                    </form>
-                                    <form action="{{url("/carrinho/del")}}" method="POST">
-                                        @csrf
-                                        @method("POST")
-                                        <input type="text" name="id_produto" value="{{$produto->pid}} hidden" hidden>
-                                        <button type="submit" class="btn btn-danger ms-3">Excluir</button>
-                                    </form>
-                                    <p class="card-text ms-auto">
-                                        <small class="fw-bold">
-                                            R$ {{number_format($produto->preco,2,",",".")}}
-                                        </small>
-                                        <small>
-                                            (R$ {{number_format(($produto->preco * $produto->qtd),2,",",".")}})
-                                        </small>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                        <span class="text-body-secondary">{{$produto->qtd}} X R$ {{number_format($produto->preco,2,",",".")}} (R$ {{number_format(($produto->preco * $produto->qtd),2,",",".")}})</span>
+                    </li>
+                @endforeach
+                <li class="list-group-item d-flex justify-content-between">
+                    <span>Total (R$)</span>
+                    <strong>{{number_format($total,2,",",".")}}</strong>
+                </li>
+            </ul>
 
-        </div>
-    
-        <div class="resumo ms-auto mb-auto p-3 rounded w-50" style="background-color: var(--light-gray);">
-            <h2>Resumo do pedido</h2>
-            <div class="d-flex mt-4">
-                <div class="flex-grow-1">({{$quantidade_produtos}}) Itens</div> 
-                <div class="">Preço total: R$ {{number_format($total,2,",",".")}}</div> 
-            </div>
-            <hr>
-            <div class="d-flex mt-4">
-                <div class="flex-grow-1">Total</div> 
-                <div class="">preço total</div> 
-            </div>
-            <form action="{{url("/carrinho/finalizar")}}" method="POST">
+            <form class="card p-2" action="{{url("/carrinho/finalizar")}}" method="POST">
                 @csrf
                 @method("POST")
-                <button class="btn mt-4 w-100" id="btn-comprar" type="submit">Comprar</button>
+                <button class="btn w-100" type="submit" style="background-color: var(--light-green);">Fazer Pedido</button>
             </form>
         </div>
 
+        <div class="col-md-7 col-lg-8">
+            <h4 class="mb-3">Produtos</h4>
+            @if (count($produtos) == 0)
+                <h5>Parece que você ainda não tem nenhum produto no carrinho. Veja o nosso <a href="{{url("/")}}">catálogo</a></h5>                
+            @endif
+            <div class="row g-3">
+                <div class="col-12">
+                    @foreach ($produtos as $produto)
+                        <div class="card mb-3" style="max-width: 1200px;">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="{{asset("storage/".$produto->imagem)}}" class="img-fluid rounded" style="height: 150px; object-fit: contain;">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{$produto->nome}}</h5>
+                                        <p class="card-text text-truncate">{{$produto->descricao}}</p>
+                                        <div class="d-flex">
+                                            <form action="{{url("/carrinho/update")}}" method="POST">
+                                                @csrf
+                                                @method("POST")
+                                                <input type="text" name="id_produto" value="{{$produto->pid}}" hidden>
+                                                <div class="dropdown">
+                                                    <button class="btn border border-1 border-dark-subtle dropdown-toggle ms-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        {{$produto->qtd}}
+                                                    </button>
+                                                    <ul class="dropdown-menu">
+                                                        @for ($i = 1; $i < $produto->pqtd; $i++)
+                                                            <li>
+                                                                <button class="dropdown-item" type="submit" name="quantidade" value="{{$i}}">
+                                                                    {{$i}}
+                                                                </button>
+                                                            </li>
+                                                            @if($i == 9)
+                                                                @break
+                                                            @endif
+                                                        @endfor
+                                                    </ul>
+                                                </div>
+                                            </form>
+                                            <form action="{{url("/carrinho/del")}}" method="POST">
+                                                @csrf
+                                                @method("POST")
+                                                <input type="text" name="id_produto" value="{{$produto->pid}} hidden" hidden>
+                                                <button type="submit" class="btn btn-danger ms-3">Excluir</button>
+                                            </form>
+                                            <p class="card-text ms-auto">
+                                                <small class="fw-bold">
+                                                    R$ {{number_format($produto->preco,2,",",".")}}
+                                                </small>
+                                                <small>
+                                                    (R$ {{number_format(($produto->preco * $produto->qtd),2,",",".")}})
+                                                </small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
-
 </div>
-
-
-<style>
-
-
-
-    #btn-comprar{
-        background-color: #00FF33;
-    }
-
-</style>
-
 
 @endsection
