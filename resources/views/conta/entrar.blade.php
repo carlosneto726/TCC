9812eb729ec791219e367f17a73a2c503e760cb4
carlosneto726 <carlosneto726@gmail.com>
2023-09-11 15:10:42 -0300

    melhorando o a pagina de entrar e de redefinir a senha

--- resources/views/conta/entrar.blade.php
+++ resources/views/conta/entrar.blade.php
@@ -1,41 +1,37 @@
-@extends('templates.template')
-@section('content')
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link href="{{asset("css/bootstrap/bootstrap.min.css")}}" rel="stylesheet">
+    <link href="{{asset("css/entrar.css")}}" rel="stylesheet">
+    <title>Cooperativas Unidas | Entrar</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-custom w-100 m-auto">
+        <form method="POST" action="{{url("/entrar")}}">
+            @csrf
+            @method("POST")
+            <img class="mb-4" src="{{asset("icons/box-arrow-in-right.svg")}}" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Entre com a sua conta</h1>
 
-<h2 class="text-center">Login</h2>
-
-<div class="p-5 mx-auto container rounded" style="width: fit-content; background-color: var(--light-gray);">
-    <h4>Faça o seu login</h4> 
-
-    <form method="POST" action="{{url("/entrar")}}">
-        @csrf
-        @method("POST")
-        <div class="mb-3">
-            <label for="inputEmail" class="form-label">E-Mail</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="digite o seu e-mail" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="inputSenha" class="form-label">Senha</label>
-            <input type="password" name="senha" class="form-control" id="inputSenha" placeholder="digite a sua senha" required>
-            <div class="form-text"><a href="{{url("/redefinir-senha")}}">Esqueceu a senha?</a></div>
-        </div>
-        
-        <button type="submit" class="btn btn-entrar" name="tipo_login" value="usuario">Entrar como usuário</button>
-        <button type="submit" class="btn btn-entrar" name="tipo_login" value="cooperativa">Entrar como cooperativa</button>
-    </form>
-
-    <hr>
-    <div>
+            <div class="form-floating">
+                <input class="form-control shadow" type="email" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="digite o seu e-mail" required>
+                <label for="inputEmail">E-Mail</label>
+            </div>
+            <div class="form-floating">
+                <input class="form-control shadow" type="password" name="senha" id="inputSenha" placeholder="digite a sua senha" required>
+                <label for="inputSenha" class="form-label">Senha</label>
+                <div class="form-text"><a href="{{url("/redefinir-senha")}}">Esqueceu a senha?</a></div>
+            </div>
+            <button class="btn btn-success w-100 m-1 shadow" type="submit" name="tipo_login" value="usuario">Entrar como usuário</button>
+            <button class="btn btn-success w-100 m-1 shadow" type="submit" name="tipo_login" value="cooperativa">Entrar como cooperativa</button>
+        </form>
+        <hr>        
         <h6 class="fw-bold text-center">Ainda não tem uma conta?</h6>
-        <a class="btn btn-entrar mt-2 w-100" href="{{url("/cadastrar")}}" style="background-color: light-green;">Cadastre-se</a>
-    </div>
-
-</div>
-
-<style>
-.btn-entrar{
-        background-color: var(--light-green);
-    }
-</style>
-
-@endsection
+        <a class="mt-2 btn btn-secondary w-100" href="{{url("/cadastrar")}}">Cadastre-se</a>
+    </main>
+</body>
+</html>
--- resources/views/conta/redefinirSenha.blade.php
+++ resources/views/conta/redefinirSenha.blade.php
@@ -1,22 +1,34 @@
-@extends('templates.template')
-@section('content')
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link href="{{asset("css/bootstrap/bootstrap.min.css")}}" rel="stylesheet">
+    <link href="{{asset("css/entrar.css")}}" rel="stylesheet">
+    <title>Cooperativas Unidas | Redefinir senha</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <main class="form-custom w-100 m-auto">
+        <form method="POST" action="{{url("/redefinir-senha")}}">
+            @csrf
+            @method("POST")
+            <img class="mb-4" src="{{asset("icons/shield-lock.svg")}}" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Redefina a sua senha</h1>
 
-<h2 class="text-center">Redefinir senha</h2>
+            <div class="form-floating">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="digite o seu e-mail" required>
+                <label for="inputEmail" class="form-label">Digite o e-mail vinculado a sua conta</label>
+            </div>
 
-<div class="p-5 mx-auto container rounded" style="width: fit-content; background-color: var(--light-gray);">
-    <h4>Redefina a sua senha</h4> 
+            <div class="form-text">Nós iremos enviar um email para você redefinir a sua senha. Não esqueça de checar o seu inbox ou a caixa de span.</div>
 
-    <form method="POST" action="{{url("/redefinir-senha")}}">
-        @csrf
-        @method("POST")
-        <div class="mb-3">
-            <label for="inputEmail" class="form-label">Digite o e-mail vinculado a sua conta</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="digite o seu e-mail" required>
-            <div class="form-text">Nós iremos enviar um email para você redefinir a sua senha.</div>
-        </div>
-
-        <button class="btn mt-2 w-100" type="submit" style="background-color: var(--light-green);">Enviar E-mail</button>
-    </form>
-</div>
-
-@endsection
+            <button class="btn btn-success mt-2 w-100" type="submit">Enviar E-mail</button>
+        </form>
+        <hr>        
+        <h6 class="fw-bold text-center">Ainda não tem uma conta?</h6>
+        <a class="mt-2 btn btn-secondary w-100" href="{{url("/cadastrar")}}">Cadastre-se</a>
+    </main>
+</body>
+</html>
